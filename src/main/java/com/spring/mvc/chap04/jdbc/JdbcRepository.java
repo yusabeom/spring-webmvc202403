00f66b712b0118791fa94ffeb456a208aec8096c
sabeom <tkqja717@naver.com>
2024-03-26 11:39:33 +0900

    #4. 전통적 방식의 JDBC part2

--- src/main/java/com/spring/mvc/chap04/jdbc/JdbcRepository.java
+++ src/main/java/com/spring/mvc/chap04/jdbc/JdbcRepository.java
@@ -3,10 +3,9 @@ package com.spring.mvc.chap04.jdbc;
 import com.spring.mvc.chap04.entity.Person;
 import org.springframework.stereotype.Repository;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Repository
 public class JdbcRepository {
@@ -82,4 +81,177 @@ public class JdbcRepository {
 
     }
 
+    // UPDATE 기능
+    public void update(Person person) {
+        Connection conn = null; // 나중에 close 하려고 미리 선언.
+
+        try {
+            // url, id, pw를 가지고 DB에 접속.
+            conn = getConnection();
+
+            // 오토커밋 끄기 (트랜잭션)
+            conn.setAutoCommit(false);
+
+            // 실행할 sql 미리 준비
+            String sql = "UPDATE person SET person_name = ?," +
+                         " person_age = ? WHERE id = ?";
+
+            // sql을 실행하는 객체를 받아오자 (작성한 sql 주자)
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+
+            // ? 를 채우자 (타입 잘 보고)
+            pstmt.setString(1, person.getPersonName());
+            pstmt.setInt(2, person.getPersonAge());
+            pstmt.setInt(3, person.getId());
+
+            // sql 실행 명령!
+            int result = pstmt.executeUpdate();
+
+            if (result == 1) conn.commit();
+            else conn.rollback();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    // DELETE 기능
+    public void delete(int id) {
+        Connection conn = null;
+
+        try {
+            conn = getConnection();
+
+            conn.setAutoCommit(false);
+
+            String sql = "DELETE FROM person WHERE id = ?";
+
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+
+            pstmt.setInt(1, id);
+
+            int result = pstmt.executeUpdate();
+
+            if (result == 1) conn.commit();
+            else conn.rollback();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    // 전체 회원 조회
+    public List<Person> findAll() {
+        List<Person> people = new ArrayList<>();
+
+        Connection conn = null;
+
+        try {
+            conn = getConnection();
+
+            String sql = "SELECT * FROM person";
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+
+            // sql 실행 -> SELECT문 -> executeQuery()
+            // ResultSet: SELECT 조회 결과를 들고 있는 객체. -> 조회 내용을 자바 타입으로 변환할 수 있는 기능 제공
+            ResultSet rs = pstmt.executeQuery();
+
+            // rs.next() 메서드는 조회 대상이 존재한다면 true를 리턴하면서 한 행을 타겟으로 잡습니다.
+            // 더 이상 조회할 데이터가 없다면 false를 리턴합니다.
+            // 만약 내가 작성한 sql의 조회 결과가 여러 행이라면 반복문을 이용해서 한 행씩 데이터를 가져옵니다.
+            // 그리고 더 이상 조회할 데이터가 없다면 반복문이 종료되도록 설계합니다.
+            while (rs.next()) {
+                // 특정 한 행이 지목된 상태 -> 행의 컬럼을 지목해서 데이터를 가져옵니다. (타입 확인 필요)
+                int id = rs.getInt("id");
+                String personName = rs.getString("person_name");
+                int personAge = rs.getInt("person_age");
+
+                Person p = new Person(id, personName, personAge);
+
+                people.add(p);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return people;
+    }
+
+    // 단일 조회 (pk 조회)
+    public Person findOne(int id) {
+        Connection conn = null;
+
+        try {
+            conn = getConnection();
+
+            String sql = "SELECT * FROM person WHERE id = ?";
+
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setInt(1, id);
+            ResultSet rs = pstmt.executeQuery();
+
+            // 가여올 데이터가 여러 행이면 반복문으로 처리
+            // 지금은 pk로 조회했기 때문에 행이 하나이거나, 없을 수 있다. -> if문으로 처리
+            if (rs.next()) {
+
+                int personId = rs.getInt("id");
+                String personName = rs.getString("person_name");
+                int personAge = rs.getInt("person_age");
+
+                return new Person(personId, personName, personAge);
+
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return null; // 조회결과가 없음을 의미
+    }
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/test/java/com/spring/mvc/chap04/jdbc/JdbcRepositoryTest.java
+++ src/test/java/com/spring/mvc/chap04/jdbc/JdbcRepositoryTest.java
@@ -1,10 +1,15 @@
 package com.spring.mvc.chap04.jdbc;
 
 import com.spring.mvc.chap04.entity.Person;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
@@ -16,9 +21,69 @@ class JdbcRepositoryTest {
     @Test
     @DisplayName("person 객체 정보를 데이터베이스에 정상 삽입해야 한다.")
     void saveTest () {
-        Person p = new Person(1, "김춘식", 30);
+        Person p = new Person(2, "홍길동", 24);
 
         repository.save(p);
+    }
+
+    @Test
+    @DisplayName("회원번호가 2번인 회원의 일므과 나이를 수정해야 한다.")
+    void updateTest() {
+        // given: 테스트를 위해 데이터를 세팅
+        int id = 2;
+        String newName = "개굴이";
+        int newAge = 15;
+        Person p = new Person(id, newName, newAge);
+        // when: 테스트 목표를 확인하여 실제 테스트가 진행되는 구간
+        repository.update(p);
+        // then: 테스트 결과를 확인하는 구간 (단언 기법 -> Assertion)
+
+    }
+
+    @Test
+    @DisplayName("회원번호가 1인 회원을 삭제해야 한다.")
+    void deleteTest() {
+        //given
+        int id = 1;
+
+        // when
+        repository.delete(id);
+    }
+
+    @Test
+    @DisplayName("더미 데이터 10개를 삽입해야 한다.")
+    void vulkInsert() {
+        for (int i = 0; i < 10; i++) {
+            Person p = new Person(0, "랄랄라" + i, i + 10);
+            repository.save(p);
+        }
+    }
+
+    @Test
+    @DisplayName("전체 회원을 조회하면 회원 리스트의 사이즈는 11일 것이다.")
+    void findAllTest() {
+        List<Person> people = repository.findAll();
+
+        for (Person person : people) {
+            System.out.println(person);
+        }
+
+        // 나는 people.size() == 11 일 것을 단언하겠다.(단언기법)
+        assertEquals(11, people.size());
+        assertNotNull(people);
+    }
+
+    @Test
+    @DisplayName("특정 아이디 회원을 조회하면 하나의 회원만 조회될 것이고," +
+                 " 없는 id를  전달하면 null이 리턴될 것이다.")
+    void findOneTest() {
+        int id = 600;
+
+        Person p = repository.findOne(id);
+        System.out.println(p);
+
+        assertNull(p);
+
     }
 
 
