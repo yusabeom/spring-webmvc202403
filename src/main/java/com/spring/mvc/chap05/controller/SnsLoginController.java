a6f3e27e5babf5844b40f004e4836b8a68b4516e
sabeom <tkqja717@naver.com>
2024-04-09 15:29:51 +0900

    git ignore config

---
+++ src/main/java/com/spring/mvc/chap05/controller/SnsLoginController.java
@@ -0,0 +1,53 @@
+package com.spring.mvc.chap05.controller;
+
+import com.spring.mvc.chap05.service.SnsLoginService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+@Slf4j
+@RequiredArgsConstructor
+public class SnsLoginController {
+
+    private final SnsLoginService snsLoginService;
+
+    @Value("${sns.kakao.app-key}")
+    private String kakaoAppKey;
+
+    @Value("${sns.kakao.redirect-uri}")
+    private String kakaoRedirectUri;
+
+    @GetMapping("/kakao/login")
+    public String kakaoLogin() {
+        String uri = "https://kauth.kakao.com/oauth/authorize";
+        uri += "?client_id=" + kakaoAppKey;
+        uri += "&redirect_uri=" + kakaoRedirectUri;
+        uri += "&response_type=code";
+
+        return "redirect:" + uri;
+    }
+
+    // 인가 코드 받기
+    @GetMapping("/auth/kakao")
+    public String snsKakao(String code) {
+        log.info("카카오 로그인 인가 코드: {}", code);
+
+        // 인가 코드를 가지고 카카오 인증 서버에 토큰 발급 요청을 보내자 (server to server 통신)
+        // 서비스에게 시킬 것임.
+        Map<String, String> params = new HashMap<>();
+        params.put("appKey", kakaoAppKey);
+        params.put("redirect", kakaoRedirectUri);
+        params.put("code", code);
+
+        snsLoginService.kakaoLogin(params);
+
+        return "";
+    }
+
+}
--- src/main/webapp/WEB-INF/views/members/sign-in.jsp
+++ src/main/webapp/WEB-INF/views/members/sign-in.jsp
@@ -96,7 +96,7 @@
                                     <td style="width: 100%; text-align: center; colspan: 2; margin-top: 24px; padding-top: 12px; border-top: 1px solid #ececec">
 
                                         <a id="custom-login-btn"
-                                           href="https://kauth.kakao.com/oauth/authorize?client_id=${kakaoAppKey}&redirect_uri=http://gondue.co.kr${kakaoRedirect}&response_type=code">
+                                           href="/kakao/login">
                                             <img src="//mud-kage.kakao.com/14/dn/btqbjxsO6vP/KPiGpdnsubSq3a0PHEGUK1/o.jpg"
                                                  width="300"/>
                                         </a>
